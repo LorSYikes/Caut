abe1301029ae146768b57f24f1f0e8105967647a
Lorran <112228338+LorSYikes@users.noreply.github.com>
2023-02-05 00:04:20 -0300

    First commit
    yayaya the first commit

---
+++ js/scriptBase.js
@@ -0,0 +1,51 @@
+function errormessage() {
+    document.getElementById("errormsg").style.visibility = "visible";
+}
+
+var response = document.getElementById("response");
+
+
+function conf1(){
+    if (response.value == "Shakespeare") {
+        window.location.href = "../lvls/a.html"
+    }
+    else{
+        errormessage();
+    }
+}
+
+function conf2(){
+    if (response.value == "Cenoura") {
+        window.location.href = "../lvls/ρ.html"
+    }
+    else{
+        errormessage();
+    }
+}
+
+function conf3(){
+    if (response.value == "Shaco") {
+        window.location.href = "../lvls/ο.html"
+    }
+    else{
+        errormessage();
+    }
+}
+
+function conf4(){
+    if (response.value == "Chuva") {
+        window.location.href = "../lvls/ύ.html"
+    }
+    else{
+        errormessage();
+    }
+}
+
+function conf5(){
+    if (response.value == "Feliz Aniversário"){
+        window.location.href = "../lvls/parabens.html"
+    }
+    else{
+        errormessage();
+    }
+}
